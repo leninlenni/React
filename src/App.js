a4781146e846f22079654ae7b265707aa30fd3c0
Ubuntu <ubuntu@ip-172-31-25-149.ec2.internal>
2019-06-06 10:45:49 +0000

    intial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>harshini</code>lenin .
         </p>
         <a
           className="App-link"
